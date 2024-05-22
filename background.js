860a57b11091b79bbae70952b883634eaff772ae
Ryan Glenn <ryanglenn415@gmail.com>
2024-05-21 23:39:45 -0700

    first test with the permission based checking system

--- background.js
+++ background.js
@@ -1,33 +1,46 @@
 chrome.action.onClicked.addListener((tab) => {
-    if (tab.url.includes("bandcamp.com")) {
-      chrome.cookies.getAll({domain: "bandcamp.com"},(cookies) => {
-        for (let cookie of cookies) {
-          console.log(`${cookie.name} at ${cookie.domain}${cookie.path}`)
-          chrome.cookies.remove({
-            url: `https://${cookie.domain}${cookie.path}`,
-            name: cookie.name
-          }, (details) => {
-            if (details) {
-              console.log(`Deleted cookie: ${cookie.name}`);
-            } else {
-              console.log(`Failed to delete cookie: ${cookie.name}`);      
-            }
-          });
-        }
-      });
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => {
-          alert("All Bandcamp cookies have been deleted.");
-        }
-      });
-    } else {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => {
-          alert("This extension only works on bandcamp.com.");
-        }
-      });
-    }
-  });
-  
+  if (tab.url.includes("bandcamp.com")) {
+    chrome.permissions.contains({
+      origins: ["https://bandcamp.com/"]
+    }, (result) => {
+      if (result) {
+        chrome.cookies.getAll({ domain: "bandcamp.com" }, (cookies) => {
+          for (let cookie of cookies) {
+            chrome.cookies.remove({
+              url: `https://${cookie.domain}${cookie.path}`,
+              name: cookie.name
+            }, (details) => {
+              if (details) {
+                console.log(`Deleted cookie: ${cookie.name}`);
+              } else {
+                console.log(`Failed to delete cookie: ${cookie.name}`);
+              }
+            });
+          }
+        });
+
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: () => {
+            alert("All Bandcamp cookies have been deleted.");
+          }
+        });
+      } else {
+        console.log("Permission not granted for https://bandcamp.com/");
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: () => {
+            alert("Permission not granted to delete Bandcamp cookies.");
+          }
+        });
+      }
+    });
+  } else {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        alert("This extension only works on bandcamp.com.");
+      }
+    });
+  }
+});
